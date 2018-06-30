228892ee2dafc108ef02bc369b9b42b059129874
Alena Messmer <alena.messmer@nordstrom.com>
2018-06-30 15:56:10 -0700

    added templates for views

--- organizer/views.py
+++ organizer/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import TodoItem
+# from django.template import loader
 
 def index(request):
     task_list = TodoItem.objects.all()[:5]
-    output = ", ".join([i.description for i in task_list])
-    return HttpResponse(output)
+    # template = loader.get_template('organizer/index.html')
+    context = {
+        'task_list': task_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'organizer/index.html', context)
 
 def detail(request, item_id):
-    return HttpResponse("todo item details: %s" % item_id)
+    # return HttpResponse("todo item details: %s" % item_id)
+    task = TodoItem.objects.get(pk=item_id)
+    return render(request, 'organizer/detail.html', {'task': task})
