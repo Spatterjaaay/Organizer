cd9fa1b41010eb8be288dbc10565950acd8d7ec9
Alena Messmer <alena.messmer@nordstrom.com>
2018-06-30 15:28:35 -0700

    added todoitem to the admin interface, added simple views

--- organizer/models.py
+++ organizer/models.py
@@ -5,3 +5,6 @@ class TodoItem(models.Model):
     description = models.CharField(max_length=256)
     time_estimate = models.IntegerField()
     due_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.description
--- organizer/views.py
+++ organizer/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoItem
 
 def index(request):
-    return HttpResponse("Hello suckers")
+    task_list = TodoItem.objects.all()[:5]
+    output = ", ".join([i.description for i in task_list])
+    return HttpResponse(output)
+
+def detail(request, item_id):
+    return HttpResponse("todo item details: %s" % item_id)
