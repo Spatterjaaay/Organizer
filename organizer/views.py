fb29650091fc3ac77b15664e22e69c521dff253d
Alena Messmer <alena.messmer@nordstrom.com>
2018-07-01 16:54:55 -0700

    removed commented out code and removed template reference

--- organizer/views.py
+++ organizer/views.py
@@ -6,32 +6,8 @@ from django.views import generic
 from .models import Task
 
 class IndexView(generic.ListView):
-    template_name = 'organizer/index.html'
-
     def get_queryset(self):
         return Task.objects.all()[:5]
 
 class DetailView(generic.DetailView):
     model = Task
-    template_name = 'organizer/detail.html'
-
-
-# def index(request):
-#     task_list = Task.objects.all()[:5]
-#     # template = loader.get_template('organizer/index.html')
-#     context = {
-#         'task_list': task_list,
-#     }
-#     # return HttpResponse(template.render(context, request))
-#     return render(request, 'organizer/index.html', context)
-#
-# def detail(request, item_id):
-#     # return HttpResponse("todo item details: %s" % item_id)
-#
-#     # very common pattern => there is a shortcut for it
-#     # try:
-#     #     task = Task.objects.get(pk=item_id)
-#     # except Task.DoesNotExist:
-#     #     raise Http404("Task does not exist")
-#     task = get_object_or_404(Task, pk=item_id)
-#     return render(request, 'organizer/detail.html', {'task': task})
