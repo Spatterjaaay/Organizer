103c040ca1037f00010f54d9a2c89c74244cb446
Alena Messmer <alena.messmer@nordstrom.com>
2018-07-07 18:04:02 -0700

    added validation for positive time estamte and no due date in the past

--- organizer/models.py
+++ organizer/models.py
@@ -1,11 +1,20 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
+
+def no_past(value):
+    today = timezone.now()
+    if value < today:
+        raise ValidationError('Date & time cannot be in the past.')
+
 
 class Task(models.Model):
     complete = models.BooleanField(default=False)
     description = models.CharField(max_length=256)
-    time_estimate = models.IntegerField()
-    due_date = models.DateTimeField()
+    time_estimate = models.IntegerField(validators=[MinValueValidator(0)])
+    due_date = models.DateTimeField(validators=[no_past])
 
     def __str__(self):
         return self.description
